553f557b2385b50a7abf38bd1d19e93950e5a11e
ayuki_j <ajjt66554@gmail.com>
2017-11-24 19:08:01 +0900

    change google api scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            scope: 'email,public_profile'
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'],
-           scope: "https://www.googleapis.com/auth/userinfo.email,https://www.googleapis.com/auth/plus.login"
+           scope: "https://www.googleapis.com/auth/userinfo.email,https://www.googleapis.com/auth/plus.login,https://www.googleapis.com/auth/profile.agerange.read, https://www.googleapis.com/auth/user.addresses.read, https://www.googleapis.com/auth/user.birthday.read"
   provider :identity,
            on_login: IdentitiesController.action(:login),
            on_registration: IdentitiesController.action(:new),
